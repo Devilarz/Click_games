d7fc9ee4d8baf1a2daa86bb16ee2ca8c04eb493b
Devilarz <ies.nalindapubudu@gmail.com>
2022-10-03 14:34:00 +0530

    Initial fill added

---
+++ ramdom_walk.py
@@ -0,0 +1,20 @@
+import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+direction = [0, 90, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
+
+for _ in range(200):
+    tim.color(random_color())
+    tim.forward(15)
+    tim.setheading(random.choice(direction))
